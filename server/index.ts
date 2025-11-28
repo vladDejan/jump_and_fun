0f032caea719483f395fb075512e295e21a071e7
vladDejan <dejanvladimirov@gmail.com>
2025-11-28 18:09:15 +0100

    Fix 0.1

--- server/index.ts
+++ server/index.ts
@@ -17,8 +17,14 @@ const port: number = Number(process.env.PORT) || 5000;
 
 // Omogućava CORS kako bi aplikacija bila dostupna iz različitih domena
 app.use(cors({
-  origin: '*',
+  origin: [
+    "https://jump-and-fun.vercel.app",  
+    "https://www.jumpandfun.rs",
+    "http://localhost:5173"
+  ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(bodyParser.json());
 
